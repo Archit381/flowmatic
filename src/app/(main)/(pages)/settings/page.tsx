757b8ca38c539a159636430e2a6c85650f7435d5
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-06-28 04:13:17 +0530

    feat: "created Infobar and init settings page"

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Settings = (props: Props) => {
+    return (
+        <div className='flex flex-col gap-4'>
+
+        </div>
+    )
+}
+
+export default Settings
