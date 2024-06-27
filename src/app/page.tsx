afd5a12eee1873040b8e3dfac1a9b1eb4551abac
kshitiz-pandey2021 <kshitiz.pandey2021@vitstudent.ac.in>
2024-06-28 03:26:52 +0530

    chore: "changed link on dashboard button"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col">
+      <Navbar/>
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
@@ -188,7 +189,7 @@ export default function Home() {
             </CardBody>
           </CardContainer>
         </div>
-      </section>      <Navbar/>
+      </section>      
     </main>
   )
 }
